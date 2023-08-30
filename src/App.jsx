b95f347624a85ef2f74022444ecb0bc399c52f8b
KateOblomova <kate.oblom@gmail.com>
2023-08-30 17:13:13 +0200

    main data

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Content from "./componentes/Content";
 
 function App() {
   return (
     <>
       {/*NavBar */}
       {/*Content (main page) */}
+      <Content />
       {/* Footer */}
       {/* Search */}
     </>
